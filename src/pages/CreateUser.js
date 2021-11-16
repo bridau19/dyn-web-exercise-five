d57b715068d667200bf15a3d3cbaca514996ff12
bridau19 <bridau19@gmail.com>
2021-11-16 08:24:10 -0500

    Add components

---
+++ src/pages/CreateUser.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CreateUserForm from "../components/CreateUserForm";
+
+function CreateUser() {
+    return (
+    <div>
+        <h1>Create Form</h1>
+        <CreateUserForm />
+    </div>
+    );
+}
+
+export default CreateUser;
